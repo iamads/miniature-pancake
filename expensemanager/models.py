ea9b5ec5e87a84e6ef3f5f624d9b81c5c989e0b6
ads <abhijeet3192@gmail.com>
2016-02-25 17:46:13 +0530

    Added dashboard and addtransaction

--- expensemanager/models.py
+++ expensemanager/models.py
@@ -1,3 +1,23 @@
 from django.db import models
-
+from datetime import datetime
 # Create your models here.
+
+class Transaction(models.Model):
+    userid = models.IntegerField()
+    name = models.CharField(max_length=300)
+    amount = models.FloatField(default=0.00)
+    category = models.CharField(max_length=50, blank=True)
+    transaction_date = models.DateTimeField(default= datetime.now)
+    is_debit = models.BooleanField(default=True)
+
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+    public = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.name
--- expensemanager/views.py
+++ expensemanager/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
-
+from django.contrib.auth.decorators import login_required
+from models import Transaction, Category
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("user id"+str(request.user.id))
 
+@login_required
+def dashboard(request):
+    userid = request.user.id
+    transactions = Transaction.objects.filter(userid=userid)
+    categories = Category.objects.all()
+    return render(request, "expensemanager/dashboard.html",{'transactions':transactions,"categories":categories})
+
+@login_required
+def addtransaction(request):
+    if request.method == "POST":
+        userid = request.user
+        name = request.POST['name']
+        amount = request.POST['amount']
+        category = request.POST['category']
+        transaction_date = request.POST['date']
+        is_debit = request.POST['is_debit']
+        transaction = Transaction.save(userid=userid, name=name , amount=amount, category=category, transaction_date=transaction_date,is_debit=is_debit)
+        transaction.save()
+        return redirect("/expensemanager/")
+    else:
+        return render(request, "expensemanager/addtransaction.html")
