8baae6b8a07e4ef71896a73e96c831929b3ff1e8
ads <abhijeet3192@gmail.com>
2016-02-26 10:12:10 +0530

    Now dashboard shows this months transaction only

--- expensemanager/models.py
+++ expensemanager/models.py
@@ -2,22 +2,23 @@ from django.db import models
 from datetime import datetime
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50,primary_key=True)
+
+
+    def __str__(self):
+        return self.name
+
 class Transaction(models.Model):
     userid= models.IntegerField()
     name = models.CharField(max_length=300)
     amount = models.FloatField(default=0.00)
-    category = models.CharField(max_length=50, blank=True)
-    transaction_date = models.DateField()   # change this to be able to select date
+    category = models.ForeignKey(Category)
+    transaction_date = models.DateField()
     is_debit = models.BooleanField(default=True)
 
 
     def __str__(self):
         return self.name
 
-class Category(models.Model):
-    name = models.CharField(max_length=50)
-    public = models.BooleanField(default=True)
-
-
-    def __str__(self):
-        return self.name
