175f6bc299cf439086d9ade97db989dc79f0a1d3
ads <abhijeet3192@gmail.com>
2016-02-26 08:39:04 +0530

    REdirected to app view

--- fulfil/urls.py
+++ fulfil/urls.py
@@ -5,7 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'fulfil.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', 'expensemanager.views.dashboard', name='dashboard'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^expensemanager/', include('expensemanager.urls')),
     url(r'^accounts/', include('registration.backends.simple.urls')),
